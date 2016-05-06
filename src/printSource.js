4693c41016b58d52a280ebb417267c679dc57ad0
Ilya Bulah <ilya.bulah@gmail.com>
2016-05-06 14:39:33 +0300

    ua

--- src/printSource.js
+++ src/printSource.js
@@ -8,6 +8,7 @@ if (!url || url == '--help') {
 }
 
 var page = require('webpage').create();
+page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36';
 
 page.open(url, function () {
     console.log(page.content);
