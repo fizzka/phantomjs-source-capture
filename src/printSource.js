58d3187a009fcaf590af8a1506c3e07c813fe3da
Ilya Bulah <ilya.bulah@gmail.com>
2016-05-06 14:39:38 +0300

    timeout

--- src/printSource.js
+++ src/printSource.js
@@ -10,7 +10,15 @@ if (!url || url == '--help') {
 var page = require('webpage').create();
 page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36';
 
-page.open(url, function () {
-    console.log(page.content);
-    phantom.exit();
+page.open(url, function (status) {
+    if (status !== 'success') {
+        console.log('Unable to load the address!');
+        phantom.exit();
+    } else {
+        window.setTimeout(function () {
+            // page.render(output);
+            console.log(page.content);
+            phantom.exit();
+        }, 2000);
+    }
 });
